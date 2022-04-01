ca02d5a95069767d855791677443483c9bdfe0f4
andrey <auledovskih@gmail.com>
2022-04-01 10:34:03 +0300

    complete task

--- src/main/java/ru/ledovskikh/andrey/amigotest/service/StudentService.java
+++ src/main/java/ru/ledovskikh/andrey/amigotest/service/StudentService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import ru.ledovskikh.andrey.amigotest.data.Student;
 import ru.ledovskikh.andrey.amigotest.data.dao.StudentRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -31,5 +33,38 @@ public class StudentService {
             throw new IllegalStateException("email taken");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        if (!studentRepository.existsById(studentId)) {
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        final Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                    "student with id " + studentId + " does not exist"));
+
+        if (name != null
+                && name.length() > 0
+                && !Objects.equals(name, student.getName())) {
+            student.setName(name);
+        }
+
+        if (email != null
+                && email.length() > 0
+                && !Objects.equals(email, student.getEmail())) {
+
+            if (studentRepository.findStudentByEmail(email).isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
